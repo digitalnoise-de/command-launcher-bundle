4e1a4d56a96bdff98f267143d21468880cdfbcff
Michael Ommen <michael.ommen.emden@gmail.com>
2023-01-13 16:42:46 +0100

    Create LaunchCommand

--- src/Command/CommandLauncherCommand.php
+++ src/Command/CommandLauncherCommand.php
@@ -3,16 +3,28 @@ declare(strict_types=1);
 
 namespace Digitalnoise\CommandLauncherBundle\Command;
 
+use Digitalnoise\CommandLauncher\CommandLauncher;
+use Digitalnoise\CommandLauncher\CommandProvider;
 use Digitalnoise\CommandLauncher\LaunchCommand;
+use Digitalnoise\CommandLauncher\ParameterResolver;
+use ReflectionException;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 final class CommandLauncherCommand extends Command
 {
-    public function __construct(private readonly LaunchCommand $command)
-    {
+    private LaunchCommand $command;
+
+    public function __construct(
+        private readonly CommandProvider $commandProvider,
+        private readonly CommandLauncher $commandLauncher,
+        /** @var list<ParameterResolver> */
+        private readonly array $parameterResolvers
+    ) {
         parent::__construct();
+
+        $this->command = new LaunchCommand($this->commandProvider, $this->commandLauncher, $this->parameterResolvers);
     }
 
     protected function configure(): void
@@ -20,7 +32,10 @@ final class CommandLauncherCommand extends Command
         $this->setName('digitalnoise:command:launch');
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+    /**
+     * @throws ReflectionException
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
         return $this->command->execute($input, $output);
     }
