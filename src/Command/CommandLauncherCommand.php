25cdfeba242c74cdafa3633370dffa73cad80080
Michael Ommen <michael.ommen.emden@gmail.com>
2023-01-13 16:37:47 +0100

    Pass correct command

--- src/Command/CommandLauncherCommand.php
+++ src/Command/CommandLauncherCommand.php
@@ -3,13 +3,14 @@ declare(strict_types=1);
 
 namespace Digitalnoise\CommandLauncherBundle\Command;
 
+use Digitalnoise\CommandLauncher\LaunchCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 final class CommandLauncherCommand extends Command
 {
-    public function __construct(private readonly CommandLauncherCommand $command)
+    public function __construct(private readonly LaunchCommand $command)
     {
         parent::__construct();
     }
