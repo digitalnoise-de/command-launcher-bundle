ccbce3a63bfb002c802011816210530c7916be02
Michael Ommen <michael.ommen.emden@gmail.com>
2023-01-13 15:27:26 +0100

    Initial

---
+++ src/CommandLauncherBundle.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace Digitalnoise\CommandLauncherBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CommandLauncherBundle extends Bundle
+{
+}
