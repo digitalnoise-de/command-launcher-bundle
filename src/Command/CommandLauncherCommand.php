78580ccce293e4184b664ae26edef8992fdf247c
Michael Ommen <michael.ommen.emden@gmail.com>
2023-01-13 15:58:15 +0100

    Add configuration

---
+++ src/Command/CommandLauncherCommand.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace Digitalnoise\CommandLauncherBundle\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+final class CommandLauncherCommand extends Command
+{
+    public function __construct(private readonly CommandLauncherCommand $command)
+    {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this->setName('digitalnoise:command:launch');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        return $this->command->execute($input, $output);
+    }
+}
