ffe86dfd0e4f073808ef62b818e7ff1b6654d0db
Michael Ommen <michael.ommen.emden@gmail.com>
2023-02-03 13:36:19 +0100

    Pass SymfonyCommandLauncher to Command

---
+++ src/CommandLauncher/SymfonyCommandLauncher.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace Digitalnoise\CommandLauncherBundle\CommandLauncher;
+
+use Digitalnoise\CommandLauncher\CommandLauncher;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+final class SymfonyCommandLauncher implements CommandLauncher
+{
+    public function __construct(private readonly MessageBusInterface $messageBus)
+    {
+    }
+
+    public function launch(object $command): void
+    {
+        $this->messageBus->dispatch($command);
+    }
+}
